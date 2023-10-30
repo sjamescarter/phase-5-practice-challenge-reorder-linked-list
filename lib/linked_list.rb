e202f4428712c2a5ac16f1277ecba5418fcb9661
sjamescarter <stephenjamescarter@gmail.com>
2023-10-30 09:49:01 -0500

    commit tree 659a0b0547395157340c5f0d5be7a7aa5737bfb1

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,3 +1,4 @@
+require 'pry'
 require_relative './node'
 
 class LinkedList
@@ -7,7 +8,64 @@ class LinkedList
     @head = head
   end
 
+  def add_node(data)
+    new_node = Node.new(data)
+
+    if @head.nil?
+      @head = new_node
+      return @head
+    end
+
+    current_node = traverse(@head)
+    current_node.next_node = new_node
+    @head
+  end
+
+  def combine(linked_list)
+    first = self
+    second = linked_list
+    current_node = traverse(@head)
+    current_node.next_node = second.head
+    first
+  end
+
+  def length
+    if @head.nil?
+      return length = 0
+    end
+
+    length = 1
+    traverse(@head) { length = length + 1 }
+
+    length
+  end
+
+  def traverse(current_node)
+    while current_node.next_node
+      yield if block_given?
+      current_node = current_node.next_node
+    end
+    current_node
+  end
+
   def reorder_linked_list
-    # your code here
+    return if @head.nil?
+
+    new_head = Node.new(@head.data)
+    odd = LinkedList.new(new_head)
+    even = LinkedList.new
+    current_node = @head
+    
+    while current_node.next_node
+      if odd.length > even.length
+        even.add_node(current_node.next_node.data)
+      else
+        odd.add_node(current_node.next_node.data)
+      end
+      current_node = current_node.next_node
+    end
+
+    odd.combine(even)
+    self.head = odd.head
   end
 end
